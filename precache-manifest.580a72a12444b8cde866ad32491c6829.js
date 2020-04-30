123be142fd466e659275de819a2d7fd79b14f276
SergijBbabich <moran.a@list.ru>
2020-04-30 17:39:37 +0300

    Updates

---
+++ precache-manifest.580a72a12444b8cde866ad32491c6829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caae9dc47ddebcc5785da4793dd5155",
+    "url": "/help_place/index.html"
+  },
+  {
+    "revision": "088407b22fbf011b2ea9",
+    "url": "/help_place/static/css/main.7e4e3ea6.chunk.css"
+  },
+  {
+    "revision": "7d4f2e1a6c977bc73cd8",
+    "url": "/help_place/static/js/2.56083841.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/help_place/static/js/2.56083841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088407b22fbf011b2ea9",
+    "url": "/help_place/static/js/main.1b8a55f2.chunk.js"
+  },
+  {
+    "revision": "f5b9bcbd9d89f428e74e",
+    "url": "/help_place/static/js/runtime-main.c79c2e97.js"
+  }
+]);
