57752d2e2b4a404f9243856d0eb8c885adf2c91d
SergijBbabich <moran.a@list.ru>
2020-04-30 18:06:29 +0300

    beta test

--- build/precache-manifest.6fe4cc9dfbc80868afc22a9fa507907f.js
+++ build/precache-manifest.6fe4cc9dfbc80868afc22a9fa507907f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4caae9dc47ddebcc5785da4793dd5155",
+    "revision": "ff7305cb2ab77054cb2c0d29de49bbe4",
     "url": "/help_place/index.html"
   },
   {
-    "revision": "088407b22fbf011b2ea9",
+    "revision": "2bc287d8d9a2f8cf04b5",
     "url": "/help_place/static/css/main.7e4e3ea6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/help_place/static/js/2.56083841.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "088407b22fbf011b2ea9",
-    "url": "/help_place/static/js/main.1b8a55f2.chunk.js"
+    "revision": "2bc287d8d9a2f8cf04b5",
+    "url": "/help_place/static/js/main.9bf34c68.chunk.js"
   },
   {
     "revision": "f5b9bcbd9d89f428e74e",
